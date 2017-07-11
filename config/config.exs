69f2ce94b0bb7e9cac7b07069430a41b6be7e5e0
Greg Mefford <greg@gregmefford.com>
2017-07-10 21:31:37 -0400

    PR feedback

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,7 @@
 use Mix.Config
 
+key_mgmt = System.get_env("NERVES_NETWORK_KEY_MGMT") || "WPA-PSK"
+
 config :nerves_network, :default,
   wlan0: [
     ssid: System.get_env("NERVES_NETWORK_SSID"),
