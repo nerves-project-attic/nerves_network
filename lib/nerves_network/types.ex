c9b334051cfd6d99b9150a4b9e48361befd94d25
Connor Rigby <connor@farmbot.io>
2017-08-29 08:23:11 -0700

    Begin adding typespecs.
    * Typed the main api.
    * Changed api of `setup/2` and `teardown/1` as per: #23
    * Begin puttinig common types in their own file.

---
+++ lib/nerves_network/types.ex
@@ -0,0 +1,9 @@
+defmodule Nerves.Network.Types do
+  @moduledoc "Types for interacting with Network."
+
+  @typedoc "Ipaddress string."
+  @type ip_address :: String.t
+
+  @typedoc "Interface name string."
+  @type ifname :: String.t
+end
