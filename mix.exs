ad36ed1340092d04a9d7489d1bd7c9bd072133ce
Connor Rigby <connor@farmbot.io>
2018-03-06 12:18:39 -0800

    Bump version prep for rc release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nerves.Network.Mixfile do
   def project do
     [
       app: :nerves_network,
-      version: "0.3.7-dev",
+      version: "0.3.7-rc0",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
