9fc47860d39c94f16a0de0967de40d1df3dbc8bc
Tomasz Kazimierz Motyl <tomasz.motyl@schneider-electric.com>
2018-03-26 03:55:57 -0700

    Providing configuration via Mix.config system

--- lib/nerves_network/application.ex
+++ lib/nerves_network/application.ex
@@ -6,10 +6,12 @@ defmodule Nerves.Network.Application do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
+    [resolvconf_file: resolvconf_file] = Application.get_env(:nerves_network, :resolver, [])
+
     children = [
       supervisor(Registry, [:duplicate, Nerves.Udhcpc], id: Nerves.Udhcpc),
       supervisor(Registry, [:duplicate, Nerves.Dhclient], id: Nerves.Dhclient),
-      worker(Nerves.Network.Resolvconf, ["/tmp/resolv.conf", [name: Nerves.Network.Resolvconf]]),
+      worker(Nerves.Network.Resolvconf, [resolvconf_file, [name: Nerves.Network.Resolvconf]]),
       supervisor(Nerves.Network.IFSupervisor, [[name: Nerves.Network.IFSupervisor]]),
       worker(Nerves.Network.Config, []),
     ]
