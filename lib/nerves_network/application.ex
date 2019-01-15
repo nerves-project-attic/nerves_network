0819fbbcdc2b22ea6296f6e0d0486ff8bbe27e17
connor rigby <konnorrigby@gmail.com>
2019-01-14 20:27:44 -0800

    Fix default config race condition.
    Thanks for the report @danielspofford

--- lib/nerves_network/application.ex
+++ lib/nerves_network/application.ex
@@ -1,16 +1,15 @@
 defmodule Nerves.Network.Application do
   @moduledoc false
-
   use Application
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
-      worker(Nerves.Network.Config, []),
       supervisor(Registry, [:duplicate, Nerves.Udhcpc]),
       worker(Nerves.Network.Resolvconf, ["/tmp/resolv.conf", [name: Nerves.Network.Resolvconf]]),
-      supervisor(Nerves.Network.IFSupervisor, [[name: Nerves.Network.IFSupervisor]])
+      supervisor(Nerves.Network.IFSupervisor, [[name: Nerves.Network.IFSupervisor]]),
+      worker(Nerves.Network.Config, [])
     ]
 
     opts = [strategy: :rest_for_one, name: Nerves.Network.Supervisor]
