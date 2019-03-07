6c8c820fe5628bc8b4ec2f3b62a27b0b7973e794
Tomasz Kazimierz Motyl <tomasz.motyl@schneider-electric.com>
2019-03-07 09:21:31 -0800

    Shortening timeouts

--- lib/nerves_network/dhclientv4_conf.ex
+++ lib/nerves_network/dhclientv4_conf.ex
@@ -319,12 +319,19 @@ defmodule Nerves.Network.Dhclientv4Conf do
   defp outermost_options_definitions() do
     #The retry statement determines the time that must pass after the client has determined that there is no DHCP server present before it tries again
     #to contact a DHCP server. By default, this is five minutes.
-    "retry 33;\n"
+    #"retry 33;\n"
     #When the client is restarted, it first tries to reacquire the last address it had. This is called the INIT-REBOOT state.
     #If it is still attached to the same network it was attached to when it last ran, this is the quickest way to get started.
     #The reboot statement sets the time that must elapse after the client first tries to reacquire its old address before it gives up
     #and tries to discover a new address. By default, the reboot timeout is ten seconds.
-    <> "reboot 9;\n"
+    #<> "reboot 9;\n"
+    """
+    timeout 33;
+    retry 33;
+    reboot 9;
+    select-timeout 3;
+    initial-interval 2;\n
+    """
   end
 
   @spec construct_contents({Types.ifname, ifmap} | any) :: String.t
